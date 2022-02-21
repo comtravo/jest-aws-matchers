66c3bb6e838b08378947789e47fddd1ab5a6b858
Puneeth-n <puneeth.nanjundaswamy@gmail.com>
2022-02-21 10:53:51 +0100

    nits

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,7 +1,2 @@
-import { stsClient } from './sts';
-
 export const AWS_REGION = process.env.AWS_REGION ?? 'eu-west-1';
-export const AWS_ACCOUNT_ID = await stsClient
-  .getCallerIdentity()
-  .promise()
-  .then((res) => res.Account);
+export const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID
